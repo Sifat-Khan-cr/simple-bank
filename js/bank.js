2ceefa2a9a8eedb6300fb25f1aafcc7957d076f4
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-08-16 16:20:44 +0600

    done

--- js/bank.js
+++ js/bank.js
@@ -11,3 +11,16 @@ document.getElementById('deposit-submit').addEventListener('click', function () 
     document.getElementById('balance').innerText = balanceTotal + deposit;
     document.getElementById('deposit-amount').value = "";
 })
+document.getElementById('withdraw-submit').addEventListener('click', function () {
+    let withdrawTotal = parseFloat(document.getElementById('withdraw').innerText);
+    const withdraw = parseFloat(document.getElementById('withdraw-amount').value);
+    const balanceTotal = parseFloat(document.getElementById('balance').innerText);
+    if (withdraw > balanceTotal) {
+        document.getElementById('withdraw-amount').value = "";
+        alert('insufficient amount');
+        return;
+    }
+    document.getElementById('withdraw').innerText = withdrawTotal + withdraw;
+    document.getElementById('balance').innerText = balanceTotal - withdraw;
+    document.getElementById('withdraw-amount').value = "";
+})
